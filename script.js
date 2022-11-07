e3bd8a9b8ab1c232e996a8eaf8dd602dba425435
fab3F <fbnhaukede@gmail.com>
2022-11-07 20:40:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let alarm = new Audio('alarm.mp3');
+    let start = false;
+    let button = document.getElementById('button');
+    let timer = document.getElementById('timer');
+    let interval;
+
+
+    function starte() {
+      if (start == false) {
+        start = true;
+        button.src = '/images/stop.png';
+        startTimer();
+      } else {
+        start = false;
+        button.src = '/images/start.png';
+        stopTimer();
+      }
+
+    }
+
+    function startTimer(){
+      let startTime = new Date().getTime();
+      let fiveMinutes = 1000 * 60 * 5;
+      let endTime = startTime + fiveMinutes;
+
+
+      interval = setInterval(function() {
+        let timeLeft = endTime - new Date().getTime();
+          if (timeLeft > 0) {
+            let minutes = timeLeft / (1000 * 60);
+            minutes = Math.floor(minutes);
+            let seconds = (timeLeft / 1000) % 60;
+            seconds = Math.round(seconds);
+            seconds = ('0' + seconds).slice(-2);
+            let text = '0'  +  minutes  +  ' : '  +  seconds;
+            timer.innerHTML = text;
+          } else {
+            if(start = true){
+              alarm.play();
+              timer.innerHTML = '05 : 00';
+              start = false;
+            }
+          }
+      }, 1000);
+    }
+    
+    
+    function stopTimer(){
+      start = false;
+      clearInterval(interval);
+      timer.innerHTML = '05 : 00';
+    }

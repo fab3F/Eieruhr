2597c613459e60d88f66f9509ea3395b29a398d4
fab3F <fbnhaukede@gmail.com>
2023-05-28 15:07:48 +0200

    minutes fix

--- script.js
+++ script.js
@@ -18,31 +18,28 @@ let alarm = new Audio('alarm.mp3');
 
     }
 
-    function startTimer(){
+    function startTimer() {
       let startTime = new Date().getTime();
       let fiveMinutes = 1000 * 60 * 5;
       let endTime = startTime + fiveMinutes;
-
-
+    
       interval = setInterval(function() {
         let timeLeft = endTime - new Date().getTime();
-          if (timeLeft > 0) {
-            let minutes = timeLeft / (1000 * 60);
-            minutes = Math.floor(minutes);
-            let seconds = (timeLeft / 1000) % 60;
-            seconds = Math.round(seconds);
-            seconds = ('0' + seconds).slice(-2);
-            let text = '0'  +  minutes  +  ' : '  +  seconds;
-            timer.innerHTML = text;
-          } else {
-            if(start = true){
-              alarm.play();
-              timer.innerHTML = '05 : 00';
-              start = false;
-              button.src = 'images/start.png';
-              clearInterval(interval);
-            }
+        if (timeLeft > 0) {
+          let minutes = Math.floor(timeLeft / (1000 * 60));
+          let seconds = Math.floor((timeLeft / 1000) % 60);
+          seconds = ('0' + seconds).slice(-2);
+          let text = ('0' + minutes).slice(-2) + ' : ' + seconds;
+          timer.innerHTML = text;
+        } else {
+          if (start = true) {
+            alarm.play();
+            timer.innerHTML = '05 : 00';
+            start = false;
+            button.src = 'images/start.png';
+            clearInterval(interval);
           }
+        }
       }, 1000);
     }
     

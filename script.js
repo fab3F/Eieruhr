4f50a343891fd0895d0891d9c3e6d9ab25e6d763
fab3F <fbnhaukede@gmail.com>
2023-10-26 17:25:00 +0200

    s

--- script.js
+++ script.js
@@ -1,56 +1,52 @@
 let alarm = new Audio('alarm.mp3');
-    let start = false;
-    let button = document.getElementById('button');
-    let timer = document.getElementById('timer');
-    let interval;
+let start = false;
+let button = document.getElementById('button');
+let timer = document.getElementById('timer');
+let interval;
 
+function starte() {
+  if (start == false) {
+    start = true;
+    button.src = 'images/stop.png';
+    startTimer();
+  } else {
+    start = false;
+    button.src = 'images/start.png';
+    stopTimer();
+  }
+}
 
-    function starte() {
-      if (start == false) {
-        start = true;
-        button.src = 'images/stop.png';
-        startTimer();
-      } else {
+function startTimer() {
+  let startTime = new Date().getTime();
+  let fiveMinutes = 1000 * 60 * 5;
+  let endTime = startTime + fiveMinutes;
+
+  let updateTimer = function() {
+    let currentTime = new Date().getTime();
+    let timeLeft = endTime - currentTime;
+
+    if (timeLeft > 0) {
+      let minutes = Math.floor(timeLeft / (1000 * 60));
+      let seconds = Math.floor((timeLeft / 1000) % 60);
+      let text = ('0' + minutes).slice(-2) + ' : ' + ('0' + seconds).slice(-2);
+      timer.innerHTML = text;
+    } else {
+      if (start === true) {
+        alarm.play();
+        timer.innerHTML = '05 : 00';
         start = false;
         button.src = 'images/start.png';
-        stopTimer();
+        clearInterval(interval);
       }
+    }
+  };
 
-    }
+  updateTimer();
+  interval = setInterval(updateTimer, 500);
+}
 
-    function startTimer() {
-      let startTime = new Date().getTime();
-      let fiveMinutes = 1000 * 60 * 5;
-      let endTime = startTime + fiveMinutes;
-    
-      let updateTimer = function() {
-        let currentTime = new Date().getTime();
-        let timeLeft = endTime - currentTime;
-    
-        if (timeLeft > 0) {
-          let minutes = Math.floor(timeLeft / (1000 * 60));
-          let seconds = Math.floor((timeLeft / 1000) % 60);
-          seconds = ('0' + seconds).slice(-2);
-          let text = ('0' + minutes).slice(-2) + ' : ' + seconds;
-          timer.innerHTML = text;
-        } else {
-          if (start === true) {
-            alarm.play();
-            timer.innerHTML = '05 : 00';
-            start = false;
-            button.src = 'images/start.png';
-            clearInterval(interval);
-          }
-        }
-      };
-    
-      updateTimer();
-      interval = setInterval(updateTimer, 500);
-    }
-    
-    
-    function stopTimer(){
-      start = false;
-      clearInterval(interval);
-      timer.innerHTML = '05 : 00';
-    }
+function stopTimer(){
+  start = false;
+  clearInterval(interval);
+  timer.innerHTML = '05 : 00';
+}
